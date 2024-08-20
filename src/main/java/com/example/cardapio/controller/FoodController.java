c208d20d0e387cf8070ce792f6ce414b4cfbc84c
Pedro Lugon <pedromotta20@gmail.com>
2024-08-20 12:52:14 -0300

    first commit

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,28 @@
+package com.example.cardapio.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @PostMapping
+    public void create() {
+
+    }
+
+    @GetMapping
+    public void getAll() {
+
+    }
+
+    @PutMapping
+    public void update() {
+
+    }
+
+    @DeleteMapping
+    public void delete() {
+
+    }
+}
